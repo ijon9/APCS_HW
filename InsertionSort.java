59aeeccc6dcd2346bd07695738962c17e3239d43
unknown <ijon@stuy.edu>
2017-12-12 17:38:41 -0500

    Insertion swap algorithm

---
+++ InsertionSort.java
@@ -0,0 +1,112 @@
+/***************************************
+ *  class InsertionSort -- implements InsertionSort algorithm
+ ***************************************/
+//Isaac Jon
+//APCS1 pd8
+//HW52 -- Poker Face
+//2017-12-13
+
+import java.util.ArrayList;
+
+public class InsertionSort
+{
+  //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al ) {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  // VOID version of InsertionSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void insertionSortV( ArrayList<Comparable> data )
+  {
+      //starts index of partition
+      for(int partition = 0; partition < data.size()-1; partition++) {
+	  for(int walkEle = partition+1; walkEle > 0; walkEle--) { //Sets element to be walked as the element right after the partition
+	      if(data.get(walkEle).compareTo(data.get(walkEle-1)) < 0)//Swaps walkEle with the element before
+		  data.set(walkEle, data.set(walkEle-1, data.get(walkEle)));
+	      else
+		  break; //Ends the for loop if it doesn't meet the condition of the swap, meaning that its in the correct position in the partition
+	  }
+      }
+  }//end insertionSortV
+
+
+  // ArrayList-returning insertionSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable>
+    insertionSort( ArrayList<Comparable> input )
+  {
+      ArrayList<Comparable> data = new ArrayList<Comparable>();
+      for(Comparable c: input)
+	  data.add(c);
+      insertionSortV(data);
+      return data;
+  }//end insertionSort
+
+
+  public static void main( String [] args )
+  {
+      /*
+      System.out.println("\n*** Testing sort-in-place (void) version... *** ");
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "\nArrayList glen before sorting:\n" + glen );
+      insertionSortV(glen);
+      System.out.println( "\nArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "\nArrayList coco before sorting:\n" + coco );
+      insertionSortV(coco);
+      System.out.println( "\nArrayList coco after sorting:\n" + coco );
+      */
+      System.out.println( "*** Testing non-void version... *** " );
+    	ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "\nArrayList glen before sorting:\n" + glen );
+      ArrayList glenSorted = insertionSort( glen );
+      System.out.println( "\nsorted version of ArrayList glen:\n" 
+      + glenSorted );
+      System.out.println( "\nArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "\nArrayList coco before sorting:\n" + coco );
+      ArrayList cocoSorted = insertionSort( coco );
+      System.out.println( "\nsorted version of ArrayList coco:\n" 
+      + cocoSorted );
+      System.out.println( "\nArrayList coco after sorting:\n" + coco );
+
+  }//end main
+
+}//end class InsertionSort
